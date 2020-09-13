4e08bab9c7fa82e60be1255b6e43bb031ad561b1
James Ferguson <james.ferguson@samsoft.se>
2020-09-13 15:36:27 +0200

    initalconfig for djangorestframework-jwt

--- itemManager/views.py
+++ itemManager/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions.IsAuthenticated
+from rest_framework.generics import ListAPIView
+
 
 # Create your views here.
+class ItemsView(ListAPIView):
+    authentication_class = (JSONWebTokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
