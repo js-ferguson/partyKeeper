c16e6fdbf828472c9c4032b360e6bc9d7ade131a
James Ferguson <james.ferguson@samsoft.se>
2020-09-13 14:08:41 +0200

    Generated a new secret key moved it to  env.py and added it to .gitignore.

--- partyKeeper/settings.py
+++ partyKeeper/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i60pdi19kqeq#0uticvp85d7qb6s=pp1j=urbs-28sv8u=my%%'
+import os
+import env
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
