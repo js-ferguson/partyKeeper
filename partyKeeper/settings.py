bf048f3ffe3a3463ca73a2284bbe9628f13b0c92
James Ferguson <jimi.windmills@gmail.com>
2020-09-19 08:46:55 +0200

    Init models and classes (#5)
    * Some initial sketches of some models and classes and how they interoperate through the api
    
    * moved models for items and inventory in the itemManager app and models for character and slots into character app

--- partyKeeper/settings.py
+++ partyKeeper/settings.py
@@ -10,26 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import datetime
+import os
 from pathlib import Path
+
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-import os
-import env
-import datetime
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'itemManager',
+    'character',
 ]
 
 MIDDLEWARE = [
@@ -61,10 +61,14 @@ JWT_AUTH = {
 
 ROOT_URLCONF = 'partyKeeper.urls'
 
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,36 +83,41 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'partyKeeper.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
+        'HOST': os.environ.get("DB_ADDRESS"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -122,7 +131,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- universe/character.py
+++ universe/character.py
@@ -1,16 +1,54 @@
 '''
 Character class aim to provide basics stats and functionality to
 player characters. It should offer an easy method of attaching item
-objects that update local variable that represent various stats of 
+objects that update local variables which represent various stats of
 the character.
 
 This class is intended to be subclassed to provide stats and abilities
 that define player characters and NPCs
 '''
 
+from universe.dice import dice as d
+
 
 class Character:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
-        self.
+    '''
+    takes a json object describing the basic character attributes
+
+    '''
+    def __init__(self, character_attr):
+        self.str = character_attr.str
+        self.dex = character_attr.dex
+        self.int = character_attr.int
+        self.char = character_attr.char
+        self.wiz = character_attr.wis
+        self.health = character_attr.health
+        self.armor = character_attr.armor
+        self.int_mod = self.dex
+
+    def calc_initiative(self, dex):
+        return d(20)
+
+    def load_item(self, item):
+        '''
+        takes a jason object with a key that specifies the type of item
+
+        should make a call either to the db or api to write that item to the
+        database item should be created in the DB and attatched to a player
+        character. Incidentally an item can also be attached to another item
+        if it contains an inventory key
+        '''
+
+    def create_slot(self, number_to_add):
+        '''
+        methods that create slots in the db and attach them
+        to character
+
+        should this be a class or a method
+        '''
+
+    def attach_to_slot(self, item):
+        '''
+        this should be a call to the api to attach an item
+        to a slot
+        '''
