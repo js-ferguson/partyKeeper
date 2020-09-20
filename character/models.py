dcb05155d9dc0b1cd8ec420d208af2cf190469b8
cronugs <cronugs@outlook.com>
2020-09-20 11:29:52 +0200

    fixing the previous commit

--- character/models.py
+++ character/models.py
@@ -1,3 +1,4 @@
+from django.core import serializers
 from django.db import models
 from itemManager.models import Inventory, Item
 
@@ -19,5 +20,21 @@ class Character(models.Model):
     str = models.IntegerField()
     ac = models.IntegerField()
     hp = models.IntegerField()
+    hit_dice = models.IntegerField(default=0)
     inventory = models.OneToOneField(Inventory, on_delete=models.CASCADE)
-    slot = models.ForeignKey(Slot, on_delete=models.CASCADE)
+    slots = models.ForeignKey(Slot, on_delete=models.CASCADE)
+
+    def __string__(self):
+        return self.name
+
+    @property
+    def get_json_ability_scores(self):
+        character = {
+            'name': self.name,
+            'dex': self.dex,
+            'str': self.str,
+            'ac': self.ac,
+            'hp': self.hp,
+        }
+
+        return serializers.serialize('json', character)
