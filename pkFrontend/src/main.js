bf0eeaf8c7a63d16f8785eeb37022b2aeff1c88c
cronugs <cronugs@outlook.com>
2020-11-02 22:13:23 +0100

    Set up the view frontend and set up an intial route. Also added some data structure and a first fake axios call

--- pkFrontend/src/main.js
+++ pkFrontend/src/main.js
@@ -4,11 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+// import axios from 'axios'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import VeeValidate from 'vee-validate'
-import library from '@fortawesome/fontawesome-svg-core'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import { ValidationProvider } from 'vee-validate'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
   faHome,
   faUser,
@@ -21,9 +22,9 @@ library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt)
 
 Vue.config.productionTip = false
 
-Vue.use(VeeValidate)
+// Vue.use(VeeValidate)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
+Vue.component('ValidationProvider', ValidationProvider)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
