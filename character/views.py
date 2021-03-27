c8183870d5afd49e7e62ac22b19d6363d4a07803
James Ferguson <jimi.windmills@gmail.com>
2021-03-27 15:08:25 +0100

    Fix jf jwt auth now working (#34)
    * auth working
    
    * replace mission ignore file

--- character/views.py
+++ character/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from universe.character import Character
 from rest_framework.response import Response
 from .models import Character as Cha_model
 
 
-def create_character(request):
-    '''
+class CreateCharacter(APIView):
+    """
+    View to create a new character in the system.
+
+    * Requires user id.
+    
     just playing around with character class and json serialization
-    '''
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        char_class = request.POST.get('class')
-        #char = Cha_model(name=request.POST.name, )
-        print(request)
-        #char.save()
-        #return char
-        content = {'name': name, 'class': char_class}
-    return Response(content)
+    """
+
+    def post(self, request, id, format=None):
+        """
+        receive data and creates a new character
+        """
+        print(id)
+        print(request.data)
+        # new_character = Character(name='request.name')
+        # usernames = [user.username for user in User.objects.all()]
+        # return Response(usernames)
+
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     char_class = request.POST.get('class')
+    #     #char = Cha_model(name=request.POST.name, )
+    #     print(request)
+    #     #char.save()
+    #     #return char
+    #     content = {'name': name, 'class': char_class}
+    # return Response(content)
 
 
 def get_characters(request):
