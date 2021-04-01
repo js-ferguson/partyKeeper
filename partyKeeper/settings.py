9d95cd56699757ccedace7c837aff649a2d7388c
James Ferguson <jimi.windmills@gmail.com>
2021-04-01 20:30:03 +0200

    I really hope this is fixed now (#39)

--- partyKeeper/settings.py
+++ partyKeeper/settings.py
@@ -18,7 +18,7 @@ from corsheaders.defaults import default_headers
 
 env = environ.Env()
 env.read_env()
-
+print(datetime.datetime.now())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,7 +73,7 @@ REST_FRAMEWORK = {
     ('rest_framework.permissions.IsAuthenticated', ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
     ),
 }
@@ -88,34 +88,62 @@ AUTHENTICATION_BACKENDS = [
 AUTH_USER_MODEL = 'users.CustomUser'
 
 from datetime import timedelta
-JWT_AUTH = {
-  'JWT_ENCODE_HANDLER':
-  'rest_framework_jwt.utils.jwt_encode_handler',
-  'JWT_DECODE_HANDLER':
-  'rest_framework_jwt.utils.jwt_decode_handler',
-  'JWT_PAYLOAD_HANDLER':
-  'rest_framework_jwt.utils.jwt_payload_handler',
-  'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-  'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-  'JWT_RESPONSE_PAYLOAD_HANDLER':
-  'rest_framework_jwt.utils.jwt_response_payload_handler',
- 
-  'JWT_SECRET_KEY': 'SECRET_KEY',
-  'JWT_GET_USER_SECRET_KEY': None,
-  'JWT_PUBLIC_KEY': None,
-  'JWT_PRIVATE_KEY': None,
-  'JWT_ALGORITHM': 'HS256',
-  'JWT_VERIFY': True,
-  'JWT_VERIFY_EXPIRATION': True,
-  'JWT_LEEWAY': 0,
-  'JWT_EXPIRATION_DELTA': timedelta(days=30),
-  'JWT_AUDIENCE': None,
-  'JWT_ISSUER': None,
-  'JWT_ALLOW_REFRESH': True,
-  'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=30),
-  'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-  'JWT_AUTH_COOKIE': None,
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+# JWT_AUTH = {
+#   'JWT_ENCODE_HANDLER':
+#   'rest_framework_jwt.utils.jwt_encode_handler',
+#   'JWT_DECODE_HANDLER':
+#   'rest_framework_jwt.utils.jwt_decode_handler',
+#   'JWT_PAYLOAD_HANDLER':
+#   'rest_framework_jwt.utils.jwt_payload_handler',
+#   'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+#   'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+#   'JWT_RESPONSE_PAYLOAD_HANDLER':
+#   'rest_framework_jwt.utils.jwt_response_payload_handler',
+ 
+#   'JWT_SECRET_KEY': 'SECRET_KEY',
+#   'JWT_GET_USER_SECRET_KEY': None,
+#   'JWT_PUBLIC_KEY': None,
+#   'JWT_PRIVATE_KEY': None,
+#   'JWT_ALGORITHM': 'HS256',
+#   'JWT_VERIFY': True,
+#   'JWT_VERIFY_EXPIRATION': True,
+#   'JWT_LEEWAY': 0,
+#   'JWT_EXPIRATION_DELTA': timedelta(days=30),
+#   'JWT_AUDIENCE': None,
+#   'JWT_ISSUER': None,
+#   'JWT_ALLOW_REFRESH': True,
+#   'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=30),
+#   'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+#   'JWT_AUTH_COOKIE': None,
+# }
 
 # JWT_AUTH = {
 #     # 'JWT_ALLOW_REFRESH': True,
@@ -212,7 +240,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Stockholm'
 
 USE_I18N = True
 
