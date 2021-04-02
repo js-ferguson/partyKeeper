ef17f0dfad29814f40bcf0f5ad6f0fdfd17a01ab
James Ferguson <jimi.windmills@gmail.com>
2021-04-02 13:50:06 +0200

    added foreign key and related_name for dm and players (#42)

--- party/models.py
+++ party/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Party(models.Model):
     name = models.CharField(max_length=30)
+    dm = models.ForeignKey(User, on_delete=models.CASCADE, related_name='dm')
+    players = models.ForeignKey(User, on_delete=models.CASCADE, related_name='players')
 
     class Meta:
         ordering = ['name']
